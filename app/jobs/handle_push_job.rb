533a602a3afe086b5b52ba9c42578002be55d4ef
Hunter Laux <hunterlaux@gmail.com>
2015-10-22 15:17:06 -0700

    test the checkout

--- app/jobs/handle_push_job.rb
+++ app/jobs/handle_push_job.rb
@@ -2,7 +2,18 @@ class HandlePushJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
+    payload = *args
     config.logger.info args
+    dir = Dir.mktmpdir
+    begin
+      # use the directory...
+      g = Git.clone(payload.head_commit.url, 'workspace', :path => dir)
+      g.checkout(payload.head_commit.id)
+      config.logger.info Dir.entries(dir)
+    ensure
+      # remove the directory.
+      FileUtils.remove_entry dir
+    end
     # Do something later
   end
 end
