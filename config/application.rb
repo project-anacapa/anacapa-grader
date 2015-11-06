23b641b3f0d6c513ec30b4701509c1ed2135fbb2
Hunter Laux <hunterlaux@gmail.com>
2015-11-05 23:37:04 -0800

    try to set git config

--- config/application.rb
+++ config/application.rb
@@ -26,3 +26,5 @@ module AnacapaGrader
     config.logger = Logger.new(STDOUT)
   end
 end
+sh 'git config --global user.email "hunterlaux+anacapabot@gmail.com"' if `git config user.email`.empty?
+sh 'git config --global user.name "AnacapaBot"' if `git config user.name`.empty?
