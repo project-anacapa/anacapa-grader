fc51eed9a6e1f089e9a11e9c0d3bcd2cf8724e25
Hunter Laux <hunterlaux@gmail.com>
2015-11-24 23:31:41 -0800

    fix grade model for json

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -14,22 +14,24 @@ class Grade
   end
 
   def process_testables(dir)
+    results_filename = "#{dir}/results/results.json"
+    expected_filename = "#{dir}/expected/expected.json"
+    results_testables = JSON.parse(File.read(results_filename))
+    expected_testables = JSON.parse(File.read(expected_filename))
+
     grade = {}
-    testables_path = "#{dir}/expected/testables"
     grade[:testables] = {}
+
     project_grade_points = 0
     project_out_of = 0
-    Dir.foreach(testables_path) do |file|
-      next if file == '.' || file == '..'
-      testable_path = "#{testables_path}/#{file}"
-      testable_name = file
-      result_path = "#{dir}/results/#{testable_name}"
-      if File.directory?(testable_path)
-        testable_results = generate_grade(testable_path,result_path)
-        grade[:testables][testable_name] = testable_results
-        project_grade_points += testable_results[:total_grade_points]
-        project_out_of += testable_results[:total_out_of]
-      end
+
+    testables = expected_testables["testables"].zip(expected_testables["testables"])
+    testables.each do |expected, results|
+      testable_results = generate_grade(expected,results)
+      grade[:testables][expected["make_target"]] = testable_results
+      project_grade_points += testable_results[:total_grade_points]
+      project_out_of += testable_results[:total_out_of]
+
     end
     grade[:project_grade_points] = project_grade_points
     grade[:project_out_of] = project_out_of
@@ -37,13 +39,11 @@ class Grade
   end
 
   #If a expected file doesn't exist generate one
-  def generate_grade(testable_path,result_path)
+  def generate_grade(expected,results)
     test = {}
-    build_results_file = "#{result_path}/build_results"
-    if File.exists?(build_results_file)
-      file = File.open(build_results_file, "r")
+    if expected["make_output"]["exit_code"] != 0
       test[:status]        = "build_failure"
-      test[:build_results] = file.read
+      test[:build_results] = expected["make_output"]["make_output"]
     else
       test[:status]        = "graded"
       test[:build_results] = "" #no need for build results if it is graded
@@ -52,36 +52,27 @@ class Grade
     total_grade_points = 0
     total_out_of  = 0
     test[:testcases] = {}
-    Dir.foreach(testable_path) do |file|
-      next if file == '.' || file == '..'
-      testcase_path = "#{testable_path}/#{file}"
-      testcase_name = file
-      if File.directory?(testcase_path)
-        json_filename = "#{testcase_path}/index.json"
-        testcase = JSON.parse(File.read(json_filename))
-        if(test[:status] == "graded")
-          expected_filename = "#{testcase_path}/expected_file"
-          result_filename = "#{result_path}/#{testcase_name}"
-          diff = Diffy::Diff.new( result_filename,expected_filename, :source => 'files')
-          grade_points = testcase ['points']
-          if diff.to_a != []
-            grade_points = 0
-          end
-          diff_string = diff.to_s(:html)
-        else
+
+    cases = expected["test_cases"].zip(results["test_cases"])
+    cases.each do |e_case, r_case|
+      diff = Diffy::Diff.new(e_case["output"], r_case["output"], :source => 'strings')
+      grade_points = e_case['points']
+      if diff.to_a != []
+        grade_points = 0
+        diff_string = diff.to_s(:html)
+      else
           grade_points = 0
           diff_string  = ""
-        end
-        out_of = testcase['points']
-        test[:testcases][testcase_name] =
-        {
+      end
+      out_of = e_case['points']
+      test[:testcases][e_case[:command]] =
+      {
           :grade_points  => grade_points,
           :out_of        => out_of,
           :diff          => diff_string
-        }
-        total_grade_points += grade_points
-        total_out_of       += out_of
-      end
+      }
+      total_grade_points += grade_points
+      total_out_of       += out_of
     end
     test[:total_grade_points] = total_grade_points
     test[:total_out_of] = total_out_of
